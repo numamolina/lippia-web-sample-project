318b4052451a7716e7496da130efd21c497f54c8
numamolina <106416047+numamolina@users.noreply.github.com>
2024-06-11 01:21:22 -0300

    TPFinal - Lippia Web - Molina Pauluk 2024

--- src/main/java/lippia/web/constants/TareasConstants.java
+++ src/main/java/lippia/web/constants/TareasConstants.java
@@ -2,7 +2,7 @@ package lippia.web.constants;
 
 public class TareasConstants {
     public static final String TITLE_INPUT_FIELD = "xpath://input[@name='autocomplete-input']";
-    public static final String CLOCK_BUTTON = "xpath://a[@data-cy='timer-mode' and @class='cl-timetracker-switch']";
+    public static final String CLOCK_BUTTON = "xpath://a[@data-cy='timer-mode' and contains(@class, 'cl-timetracker-switch')]";
     public static final String START_BUTTON = "xpath://button[contains(text(), 'Start')]";
     public static final String STOP_BUTTON = "xpath://button[contains(text(), 'Stop')]";
     public static final String TASK_TOAST = "xpath://*[@id='toast-container' and contains(@class, 'toast-bottom-right')]";
